ba94998b8111d1f545524e4b99ad87a0ff59d96d
Joey Hess <joeyh@joeyh.name>
2016-06-17 14:51:41 -0400

    Support git's new smudgeToFile/cleanFromFile interface.
    Using this interface makes v6 mode more efficient, and makes annex.thin be
    honored when git updates unlocked files in the work tree.

--- Command/Smudge.hs
+++ Command/Smudge.hs
@@ -27,27 +27,29 @@ cmd = noCommit $ noMessages $
 		"git smudge filter"
 		paramFile (seek <$$> optParser)
 
-data SmudgeOptions = SmudgeOptions
-	{ smudgeFile :: FilePath
+data CmdOptions = CmdOptions
+	{ workTreeFile :: FilePath
 	, cleanOption :: Bool
+	, directAccessOption :: Bool
 	}
 
-optParser :: CmdParamsDesc -> Parser SmudgeOptions
-optParser desc = SmudgeOptions
+optParser :: CmdParamsDesc -> Parser CmdOptions
+optParser desc = CmdOptions
 	<$> argument str ( metavar desc )
 	<*> switch ( long "clean" <> help "clean filter" )
+	<*> switch ( long "direct-access" <> help "direct file access (for smudgeToFile and cleanFromFile)")
 
-seek :: SmudgeOptions -> CommandSeek
+seek :: CmdOptions -> CommandSeek
 seek o = commandAction $
-	(if cleanOption o then clean else smudge) (smudgeFile o)
+	(if cleanOption o then clean else smudge) o
 
--- Smudge filter is fed git file content, and if it's a pointer to an
--- available annex object, should output its content.
-smudge :: FilePath -> CommandStart
-smudge file = do
+-- Smudge filter is fed git file content on stdin, and if it's a
+-- pointer to an available annex object, should provide its content.
+smudge :: CmdOptions -> CommandStart
+smudge o = do
 	b <- liftIO $ B.hGetContents stdin
 	case parseLinkOrPointer b of
-		Nothing -> liftIO $ B.putStr b
+		Nothing -> providecontent b
 		Just k -> do
 			Database.Keys.addAssociatedFile k =<< inRepo (toTopFilePath file)
 			-- A previous unlocked checkout of the file may have
@@ -57,35 +59,56 @@ smudge file = do
 			-- modifications.
 			ifM (inAnnex k)
 				( do
-					content <- calcRepo (gitAnnexLocation k)
-					whenM (annexThin <$> Annex.getGitConfig) $
-						warning $ "Not able to honor annex.thin when git is checking out " ++ file ++ " (run git annex fix to re-thin files)"
-					liftIO $ B.putStr . fromMaybe b
-						=<< catchMaybeIO (B.readFile content)
-				, liftIO $ B.putStr b
+					contentfile <- calcRepo (gitAnnexLocation k)
+					thin <- annexThin <$> Annex.getGitConfig
+					if thin && directAccessOption o
+						then do
+							r <- linkFromAnnex k file Nothing
+							case r of
+								LinkAnnexFailed -> providecontent b
+								_ -> return ()
+						else do
+							when thin $
+								warning $ "Not able to honor annex.thin when git is checking out " ++ file ++ " (run git annex fix to re-thin files)"
+							providecontent . fromMaybe b
+								=<< liftIO (catchMaybeIO (B.readFile contentfile))
+				, providecontent b
 				)
 	stop
+  where
+	providecontent b
+		| directAccessOption o = liftIO $ B.writeFile file b
+		| otherwise = liftIO $ B.putStr b
+	file = workTreeFile o
 
--- Clean filter is fed file content on stdin, decides if a file
--- should be stored in the annex, and outputs a pointer to its
--- injested content.
-clean :: FilePath -> CommandStart
-clean file = do
-	b <- liftIO $ B.hGetContents stdin
+-- Clean filter is fed file content, decides if a file should be stored in
+-- the annex, and outputs a pointer to its injested content.
+clean :: CmdOptions -> CommandStart
+clean o = do
+	b <- liftIO $ if directAccessOption o
+		then B.readFile file
+		else B.hGetContents stdin
 	if isJust (parseLinkOrPointer b)
 		then liftIO $ B.hPut stdout b
 		else ifM (shouldAnnex file)
 			( do
-				-- Even though we ingest the actual file,
+				-- Even though we ingest the worktree file,
 				-- and not stdin, we need to consume all
 				-- stdin, or git will get annoyed.
-				B.length b `seq` return ()
+				unless (directAccessOption o) $
+					B.length b `seq` return ()
 				-- Look up the backend that was used
 				-- for this file before, so that when
 				-- git re-cleans a file its backend does
 				-- not change.
 				currbackend <- maybe Nothing (maybeLookupBackendName . keyBackendName)
 					<$> catKeyFile file
+				-- Ingesting the worktree file is really
+				-- only safe when git runs the smudgeToFile
+				-- filter. But in practice, the smudge
+				-- filter is only run on worktree files,
+				-- so we can get away with directly
+				-- accessing the file.
 				liftIO . emitPointer
 					=<< go
 					=<< ingest' currbackend
@@ -94,6 +117,7 @@ clean file = do
 			)
 	stop
   where
+	file = workTreeFile o
 	go (Just k, _) = do
 		logStatus k InfoPresent
 		return k
